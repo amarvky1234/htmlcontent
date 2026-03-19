d22d4ff63bbcca1490d4efb0bad1bb86c6676acf
Amarjeet Kumar <amarvky1234@gmail.com>
2026-03-19 10:14:45 +0530

    added js files

---
+++ myscript.js
@@ -0,0 +1,7 @@
+/*example on creating external script*/
+function myfun1(){
+    document.write("hi, this fun1 from external script <br>"+ "<br>");
+}
+function myfun2(){
+    document.write("hello, this fun2 from external script");
+}
